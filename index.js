e7369759c89942ba7a905a21f73670e57922e0ec
halla <aniabisso.16@gmail.com>
2023-07-23 20:54:46 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+
+  const gifts = ["Quby", "Ruby", "Ali"];
+  const event = "birthday";
+  const countdownStart = 10;
+
+  const thankYouMessages = writeCards(gifts, event);
+  console.log(thankYouMessages);
+
+  countDown(countdownStart);
+
